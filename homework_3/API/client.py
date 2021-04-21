d8ca24a98c09c08c75f88f90743c7166d9308733
Ilya <60218057+Froztgal@users.noreply.github.com>
2021-04-21 12:56:32 +0300

    Fix for Homework 3.

--- homework_3/API/client.py
+++ homework_3/API/client.py
@@ -29,7 +29,7 @@ class ApiClient:
 
         if response.status_code != expected_status:
             raise ResponseStatusCodeException(f'Got {response.status_code} '
-                                              f'{response.reason} for URL ' 
+                                              f'{response.reason} for URL '
                                               f'"{response.url}"!\n'f'Expected status_code: {expected_status}.')
 
         if jsonify:
@@ -71,52 +71,66 @@ class ApiClient:
         result = self.session.get(url, headers=headers)
         self._response_check(result, jsonify=False)
 
-    def post_create_company(self, name):
+    def get_url_company(self, ad_url):
+
+        location = '/api/v1/urls/'
+        url = self._make_url(location)
+
+        data = {'url': ad_url}
+
+        result = self.session.get(url, params=data)
+        self._response_check(result)
+
+        return result.json()['id']
+
+    def post_upload_image_company(self, path):
+
+        location = '/api/v2/content/static.json'
+        url = self._make_url(location)
+
+        headers = {'X-CSRFToken': self.session.cookies.get('csrftoken')}
+
+        files = {"file": open(path, "rb")}
+
+        result = self.session.post(url, headers=headers, files=files)
+        self._response_check(result)
+
+        img_id = result.json()['id']
+
+        location = '/api/v2/mediateka.json'
+        url = self._make_url(location)
+
+        data = {
+            "description": "test_img.jpg",
+            "content": {"id": img_id}
+        }
+
+        self.session.post(url, headers=headers, json=data)
+
+        return img_id
+
+    def post_create_company(self, company, path_json):
 
         location = '/api/v2/campaigns.json'
         url = self._make_url(location)
 
         headers = {'X-CSRFToken': self.session.cookies.get('csrftoken')}
 
-        data = '{"name": "' + f'{name}", ' + """
-                    "conversion_funnel_id":null,"objective":"traffic",
-                    "enable_offline_goals":false,"targetings":{
-                    "split_audience":[1,2,3,4,5,6,7,8,9,10],"sex":["male",
-                    "female"],"age":{"age_list":[0,12,13,14,15,16,17,18,19,
-                    20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,
-                    39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,
-                    58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75],
-                    "expand":true},"geo":{"regions":[188]},
-                    "interests_soc_dem":[],"segments":[],"interests":[],
-                    "fulltime":{"flags":["use_holidays_moving",
-                    "cross_timezone"],"mon":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,
-                    14,15,16,17,18,19,20,21,22,23],"tue":[0,1,2,3,4,5,6,7,8,
-                    9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],"wed":[0,1,
-                    2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
-                    23],"thu":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
-                    18,19,20,21,22,23],"fri":[0,1,2,3,4,5,6,7,8,9,10,11,12,
-                    13,14,15,16,17,18,19,20,21,22,23],"sat":[0,1,2,3,4,5,6,7,
-                    8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],"sun":[0,
-                    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
-                    23]},"pads":[102643],"mobile_types":["tablets",
-                    "smartphones"],"mobile_vendors":[],"mobile_operators":[
-                    ]},"age_restrictions":null,"date_start":null,
-                    "date_end":null,"autobidding_mode":"second_price_mean",
-                    "budget_limit_day":null,"budget_limit":null,
-                    "mixing":"fastest","utm":null,"enable_utm":true,
-                    "price":"17.11","max_price":"0","package_id":961,
-                    "banners":[{"urls":{"primary":{"id":14570324}},
-                    "textblocks":{},"content":{"image_240x400":{
-                    "id":8652017}},"name":""}]}
-                    """
+        with open(path_json, 'r') as f:
+            data = json.loads(f.read())
 
-        result = self.session.post(url, headers=headers, json=json.loads(data))
+        data['name'] = company.name
+        data['banners'][0]['content']['image_240x400']['id'] = company.img_id
+        data['banners'][0]['urls']['primary']['id'] = company.url_id
 
-        return json.loads(result.content.decode('utf-8'))['id']
+        result = self.session.post(url, headers=headers, json=data)
+        self._response_check(result)
 
-    def get_check_company(self, company_id):
+        return result.json()['id']
 
-        location = f'/api/v2/campaigns/{company_id}.json'
+    def get_check_company(self, company):
+
+        location = f'/api/v2/campaigns/{company.company_id}.json'
         url = self._make_url(location)
 
         headers = {'X-CSRFToken': self.session.cookies.get('csrftoken')}
@@ -125,14 +139,14 @@ class ApiClient:
 
         return result.status_code
 
-    def post_delete_company(self, company_id):
+    def post_delete_company(self, company):
 
         location = '/api/v2/campaigns/mass_action.json'
         url = self._make_url(location)
 
         headers = {'X-CSRFToken': self.session.cookies.get('csrftoken')}
 
-        data = [{"id": company_id, "status": "deleted"}]
+        data = [{"id": company.company_id, "status": "deleted"}]
 
         result = self.session.post(url, headers=headers, json=data)
         self._response_check(result, expected_status=204, jsonify=False)
@@ -150,14 +164,13 @@ class ApiClient:
             'logicType': "or",
             'name': name,
             'pass_condition': 1,
-            'relations': [{'object_type': 'remarketing_player',
-                           'params': {'type': 'positive', 'left': 365,
-                                      'right': 0}}]
+            'relations': [{'object_type': 'remarketing_player', 'params': {'type': 'positive', 'left': 365,
+                                                                           'right': 0}}]
         }
 
         result = self.session.post(url, headers=headers, json=data)
         self._response_check(result)
-        return json.loads(result.content.decode('utf-8'))['id']
+        return result.json()['id']
 
     def get_check_segment(self, segment_id):
 
--- homework_3/tests_api/test.py
+++ homework_3/tests_api/test.py
@@ -1,17 +1,27 @@
+import os
 import data
 import pytest
 from tests_api.base import ApiBase
+from utils.builder import Builder as CompanyBuilder
 
 
 class CompanyBase(ApiBase):
 
     @pytest.fixture(scope='function')
-    def company(self):
-        name = data.get_random_string()
-        company_id = self.api_client.post_create_company(name=name)
-        yield company_id
-        self.api_client.post_delete_company(company_id)
-        self.api_client.get_check_company(company_id)
+    def file_path_img(self, repo_root):
+        return os.path.join(repo_root, 'ui', 'test_img.jpg')
+
+    @pytest.fixture(scope='function')
+    def file_path_json(self, repo_root):
+        return os.path.join(repo_root, 'API', 'company_template.json')
+
+    @pytest.fixture(scope='function')
+    def company(self, file_path_img, file_path_json):
+        new_company = CompanyBuilder().create_company(self.api_client, img_path=file_path_img)
+        new_company.company_id = self.api_client.post_create_company(new_company, path_json=file_path_json)
+        yield new_company
+        self.api_client.post_delete_company(new_company)
+        self.api_client.get_check_company(new_company)
 
 
 @pytest.mark.API
