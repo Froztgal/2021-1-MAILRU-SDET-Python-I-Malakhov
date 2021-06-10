e05bc62defd1cc5ea985c3b65ce31680e1dd4635
Ilya <60218057+Froztgal@users.noreply.github.com>
2021-06-10 12:40:34 +0300

    Fix for FP.

--- FP/data.py
+++ FP/data.py
@@ -3,5 +3,5 @@ import string
 
 
 def get_random_string():
-    string_length = random.randint(6, 16)
+    string_length = random.randint(6, 10)
     return ''.join(random.choices(string.ascii_letters + string.digits, k=string_length))
